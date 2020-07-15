c937e8ff5d6172c4ffcea52a71f5d870ddf92d21
Antonio Fortino <thoniorf@gmail.com>
2020-07-15 21:46:32 +0200

    Added new parameters

--- lib/view/utils/Parameters.dart
+++ lib/view/utils/Parameters.dart
@@ -1,3 +1,11 @@
 class Parameters {
-  final String TODAY = "OGGI";
+  static const String TODAY = "Oggi";
+
+  static const String EMPTY_LIST = "Non hai aggiunto nessuna misurazione.";
+
+  static const String BLOOD_PRESSURE_MIN = "Min";
+  static const String BLOOD_PRESSURE_MAX = "Max";
+  static const String BLOOD_PRESSURE_PUL = "Pul";
+
+  static const String ADD_NEW_MEASURMENT = "Nuovo";
 }
