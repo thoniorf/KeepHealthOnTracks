a015bff5fd4c1f25bcc6b66dd108cfaf4ab5c094
Antonio Fortino <thoniorf@gmail.com>
2020-07-15 21:45:55 +0200

    added single day page with measurements

---
+++ lib/view/DayDisplayPage/DayDisplayPage.dart
@@ -0,0 +1,96 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:keep_health_on_track/model/BloodPressure.dart';
+import 'package:keep_health_on_track/model/Measurement.dart';
+import 'package:keep_health_on_track/view/DayDisplayPage/MeasurementListTile.dart';
+import 'package:keep_health_on_track/view/utils/Parameters.dart';
+
+class DayDisplayPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DayDisplayPageState();
+}
+
+class DayDisplayPageState extends State<DayDisplayPage> {
+  DateTime _day;
+  List<Measurement> _measurements;
+  String _appBarTitle;
+
+  DayDisplayPageState([this._day]) {
+    this._day ??= DateTime.now();
+    this._measurements = List();
+    _appBarTitle = computeAppTitle(this._day);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int _itemCount = _measurements.length == 0 ? 1 : _measurements.length;
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          title: Text(
+            this._appBarTitle.toUpperCase(),
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: IconButton(
+              icon: Icon(Icons.calendar_view_day, color: Colors.black),
+              onPressed: null),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.add, color: Colors.black),
+                onPressed: addMeasurement)
+          ],
+        ),
+        body: Container(
+          margin: EdgeInsets.only(top: 24.0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+              color: Color(0xFFF9EDE9),
+              shape: BoxShape.rectangle),
+          child: ListView.builder(
+              padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0),
+              itemCount: _itemCount,
+              itemBuilder: listItemBuilder),
+        ),
+      ),
+    );
+  }
+
+  void addMeasurement() {
+    _measurements.add(Measurement(BloodPressure(Random().nextInt(20) + 60,
+        Random().nextInt(40) + 100, Random().nextInt(50) + 40)));
+    setState(() {});
+  }
+
+  Widget listItemBuilder(context, index) {
+    if (_measurements.isEmpty)
+      return ListTile(
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            Parameters.EMPTY_LIST,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+    return MeasurementListTile(_measurements[index]);
+  }
+
+  String computeAppTitle(DateTime day) {
+    DateTime now = DateTime.now();
+    if (day.year == now.year && day.month == now.month && day.day == now.day) {
+      return Parameters.TODAY;
+    } else {
+      return DateFormat("dd MMMM yyyy").format(day);
+    }
+  }
+}
